3b379c5f453da7ad78e6f5fd1e517282abce7dff
MilesAngels <mangels@alphaworks.tech>
2022-03-10 11:07:28 -0800

    adding stuff to dropdown

---
+++ js/dropdown-nav.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).slideDown(100);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).slideUp(200);
+    });
+});
